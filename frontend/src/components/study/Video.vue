537e52708886010ff87febe5d363e1ae4076b399
olrlozl <thakd221@naver.com>
2024-03-13 13:30:33 +0900

    Feat: Video 컴포넌트 생성

---
+++ frontend/src/components/study/Video.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { storeToRefs } from 'pinia'
+import { useStudyStore } from '@/stores/studyStore.js'
+const studyStore = useStudyStore()
+const { videoData, curSentence } = storeToRefs(studyStore)
+
+const getVideoUrl = (videoId, startTime) => {
+    return `https://www.youtube.com/embed/${videoId}?start=${startTime}&autoplay=1&rel=0`
+}
+
+</script>
+
+<template>
+    <div class="video">
+        <iframe width="100%" height="auto"
+            :src="getVideoUrl(videoData.videoId, curSentence.startTime)"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share;"
+            allowfullscreen></iframe>
+    </div>
+</template>
+
+<style scoped>
+.video {
+    position: relative;
+    width: 100%;
+    padding-bottom: 56.25%;
+    margin-bottom: 15px;
+}
+.video iframe {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    border-radius: 5px;
+}
+</style>
