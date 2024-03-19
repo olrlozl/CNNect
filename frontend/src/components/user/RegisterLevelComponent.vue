1a5f697121204db6dbf78f45cbc6a09ebe90cd91
Haeun Kim <gkdms1120s@gmail.com>
2024-03-19 17:42:22 +0900

    Feat: 레벨 테스트 상세 로직 구현

--- frontend/src/components/user/RegisterLevelComponent.vue
+++ frontend/src/components/user/RegisterLevelComponent.vue
@@ -1,19 +1,22 @@
 <template>
-  <h3>제시 된 단어의 의미를 선택해주세요</h3>
-  <RegisterLevelDetail :stage="steps[level - 1]"></RegisterLevelDetail>
-  <div class="flex items-center w-full justify-center">
-    <button
-      @click="nextStep(2)"
-      class="items-center mx-3 text-white bg-gray-400 hover:bg-gray-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
-    >
-      그만두기
-    </button>
-    <button
-      @click="level < 6 ? level++ : registerComplete()"
-      class="items-center text-white bg-theme-red hover:bg-theme-redbrown focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
-    >
-      {{ level < 6 ? "다음" : "제출" }}
-    </button>
+  <div class="flex flex-col justify-center h-full w-full items-center">
+    <h3>Lv. {{ level }} 제시 된 단어의 의미를 선택해주세요</h3>
+    <RegisterLevelDetail :stage="steps[level - 1]" ref="registerLevelDetail" />
+
+    <div class="flex items-center w-full justify-center">
+      <button
+        @click="nextStep(2)"
+        class="mx-3 text-white bg-gray-400 hover:bg-gray-500 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5"
+      >
+        그만두기
+      </button>
+      <button
+        @click="nextLevel"
+        class="text-white bg-theme-red hover:bg-theme-redbrown focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5"
+      >
+        {{ level < 6 ? "다음" : "제출" }}
+      </button>
+    </div>
   </div>
 </template>
 
@@ -23,11 +26,28 @@ import RegisterLevelDetail from "@/components/user/RegisterLevelDetailComponent.
 
 const steps = ref(["A1", "A2", "B1", "B2", "C1", "C2"]);
 const level = ref(1);
+const registerLevelDetail = ref(null);
 const emit = defineEmits(["updateLevel"]);
 
 watch(level, (newValue) => {
+  // 'updateLevel' 이벤트를 발생시키고, 새로운 level 값을 전달합니다.
   emit("updateLevel", newValue);
 });
-</script>
 
-<style></style>
+const nextLevel = () => {
+  const wrongCount = registerLevelDetail.value.checkAnswers();
+
+  if (wrongCount > 2) {
+    alert("통과 불가");
+    // updateLevel() <- 유저의 레벨 업데이트
+  } else {
+    console.log(level.value);
+    if (level.value == 6) {
+      alert("최종 레벨");
+      // updateLevel() <- 유저의 레벨 업데이트
+    } else {
+      level.value++;
+    }
+  }
+};
+</script>
--- frontend/src/components/user/RegisterLevelDetailComponent.vue
+++ frontend/src/components/user/RegisterLevelDetailComponent.vue
@@ -1,32 +1,28 @@
 <template>
-  {{ stage }}
-  <!-- <div v-for="(item, index) in data" :key="item.word_id" class="mb-4">
-    {{ item.word_content }}
-    <div class="flex flex-wrap items-center justify-center mt-2">
-      <label
-        class="inline-flex items-center mx-2"
-        v-for="(mean, meanIndex) in item.word_mean"
-        :key="meanIndex"
-      >
-        <input
-          type="radio"
-          name="word-meaning-{{ item.word_id }}"
-          class="form-radio"
-          :value="mean"
-          v-model="selectedAnswers[index]"
-        />
-        <span class="ml-2">{{ mean }}</span>
-      </label>
+  <div v-for="item in data" :key="item.word_id" class="w-[60%] flex mb-4">
+    <div class="w-1/2 text-center">
+      {{ item.word_content }}
+    </div>
+    <div class="w-1/2 flex items-center justify-center">
+      <p class="w-3/8 h-1"></p>
+      <div class="w-5/8 text-center">
+        <label
+          class="block items-center mr-2 text-start"
+          v-for="(mean, meanIndex) in item.word_mean"
+          :key="meanIndex"
+        >
+          <input
+            type="radio"
+            :name="'word-meaning-' + item.word_id"
+            class="form-radio"
+            :value="mean"
+            v-model="selectedAnswers[item.word_id]"
+          />
+          <span class="ml-2">{{ mean }}</span>
+        </label>
+      </div>
     </div>
   </div>
-  <div class="flex items-center justify-center mt-5">
-    <button
-      @click="checkAnswers"
-      class="mx-3 text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
-    >
-      제출
-    </button>
-  </div> -->
 </template>
 
 <script setup>
@@ -34,6 +30,57 @@ import { ref } from "vue";
 const props = defineProps({
   stage: String,
 });
-</script>
+const emit = defineEmits(["wrongCountUpdated"]);
 
-<style></style>
+const data = ref([
+  {
+    word_id: 1,
+    word_content: "apple",
+    word_answer: "사과",
+    word_mean: ["사과", "오렌지", "복숭아", "수박"],
+  },
+  {
+    word_id: 2,
+    word_content: "dog",
+    word_answer: "개",
+    word_mean: ["개", "고양이", "토끼", "호랑이"],
+  },
+  {
+    word_id: 3,
+    word_content: "apple",
+    word_answer: "사과",
+    word_mean: ["사과", "오렌지", "복숭아", "수박"],
+  },
+  {
+    word_id: 4,
+    word_content: "dog",
+    word_answer: "개",
+    word_mean: ["개", "고양이", "토끼", "호랑이"],
+  },
+  {
+    word_id: 5,
+    word_content: "dog",
+    word_answer: "개",
+    word_mean: ["개", "고양이", "토끼", "호랑이"],
+  },
+]);
+
+const selectedAnswers = ref({});
+
+const checkAnswers = () => {
+  let count = 0;
+  data.value.forEach((item) => {
+    if (selectedAnswers.value[item.word_id] !== item.word_answer) {
+      count++;
+    }
+  });
+  return count;
+};
+
+defineExpose({ checkAnswers });
+
+// 각 항목에 대해 초기 선택값을 null로 설정
+data.value.forEach((item) => {
+  selectedAnswers.value[item.word_id] = null;
+});
+</script>
