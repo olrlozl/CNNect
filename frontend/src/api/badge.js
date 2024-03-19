47fb1d09836118e5811e4208332d6d8e99637c3f
이민주 <gabs3798@naver.com>
2024-03-19 17:35:44 +0900

    Feat : 마이페이지 뱃지 조회 화면 구성

---
+++ frontend/src/api/badge.js
@@ -0,0 +1,22 @@
+import { localAxios } from "./http";
+
+const local = localAxios();
+const url = "/badge";
+const config = {
+    headers : {
+        "Authorization" : "Bearer " + localStorage.getItem("accessToken")
+    }
+} // 헤더에 accessToken 담아서 전송하기!!
+
+async function allBadges(success, fail){
+    local.get(`${url}/alllist`).then(success).catch(fail);
+}
+
+async function myBadges(success, fail){
+    local.get(`${url}/mylist`, config).then(success).catch(fail);
+}
+
+export {
+    allBadges,
+    myBadges
+}
--- frontend/src/views/user/MyPageView.vue
+++ frontend/src/views/user/MyPageView.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="view-frame flex items-start">
-    <div id="profile" class="w-1/3">
+  <div class="view-frame flex items-start overflow-y-auto">
+    <div id="profile" class="w-1/2 sticky top-0">
       <!-- <span class="nav-title"> 프로필 </span> -->
       <div
         class="dark:!bg-navy-800 shadow-shadow-500 shadow-3xl rounded-primary relative mx-auto flex h-full w-full max-w-[550px] flex-col items-center bg-white bg-cover bg-clip-border p-[16px] dark:text-white dark:shadow-none"
@@ -12,11 +12,7 @@
           <div
             class="absolute -bottom-12 flex h-[88px] w-[88px] items-center justify-center rounded-full border-[4px] border-white bg-pink-400"
           >
-            <img
-              class="h-full w-full rounded-full"
-              :src="imgUrl"
-              alt=""
-            />
+            <img class="h-full w-full rounded-full" :src="imgUrl" alt="" />
           </div>
         </div>
         <div class="mt-16 flex flex-col items-center">
@@ -28,11 +24,11 @@
             <p class="text-lightSecondary text-sm font-normal">Level</p>
           </div>
           <div class="flex flex-col items-center justify-center">
-            <h3 class="text-bluePrimary text-2xl font-bold">9.7K</h3>
+            <h3 class="text-bluePrimary text-2xl font-bold">{{ badge }}</h3>
             <p class="text-lightSecondary text-sm font-normal">Badge</p>
           </div>
           <div class="flex flex-col items-center justify-center">
-            <h3 class="text-bluePrimary text-2xl font-bold">434</h3>
+            <h3 class="text-bluePrimary text-2xl font-bold">{{ video }}</h3>
             <p class="text-lightSecondary text-sm font-normal">Study</p>
           </div>
         </div>
@@ -60,8 +56,21 @@
         </button>
       </div>
     </div>
-    <div id="badge" class="w-2/3">
-      <span class="nav-title"> 획득 뱃지 </span>
+    <div id="badge" class="w-1/2 flex flex-col items-center justify-center">
+      <span class="nav-title mb-5"> 획득 뱃지 </span>
+      <div class="flex-container items-center justify-center">
+        <div v-for="badge in badgeList" :key="badge.badgeId" class="flex-item items-center justify-center">
+          <div
+            class="flex border rounded-full w-32 h-32 justify-center items-center "
+            :class="{'filter grayscale bg-gray-200 opacity-40' : badge.check == false,
+                      'border-4 border-yellow-500 bg-yellow-300' : badge.tier == 1
+                    }"
+          >
+            <img class="flex" :src="badge.url" 
+            />
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -70,16 +79,86 @@
 import { userStore } from "@/stores/userStore";
 import { storeToRefs } from "pinia";
 import { onMounted, ref } from "vue";
+import { userInfo } from "@/api/user";
+import { allBadges, myBadges } from "@/api/badge";
 
 const uStore = userStore();
 
 const imgUrl = ref("");
 const { nickName, level } = storeToRefs(uStore);
+const badge = ref(0);
+const video = ref(0);
+
+const badgeList = ref([]);
+const userBadge = ref([]);
 
 onMounted(() => {
   imgUrl.value = "/public/level/level" + level.value + ".png";
-})
+  userInfo(
+    ({ data }) => {
+      console.log(data);
+      badge.value = data.data.userBadgeCount;
+      video.value = data.data.userVideoCount;
+    },
+    (error) => {
+      console.log(error);
+    }
+  );
 
+  myBadges(
+    ({ data }) => {
+      console.log(data);
+      userBadge.value = data.data;
+      allBadges(
+        ({ data }) => {
+          console.log(data);
+          for (let i = 0; i < data.data.length; i++) {
+            let condition = data.data[i].badgeCondition;
+            let category = data.data[i].badgeCategory;
+            let url =
+              condition == "VIDEO"
+                ? "/public/badge/" + condition + "/" + category + ".png"
+                : "/public/badge/" + condition + "/" + condition + ".png";
+            let check = userBadge.value.some((badge) => {
+              return JSON.stringify(badge) === JSON.stringify(data.data[i]);
+            });
+            let tier = data.data[i].badgeId % 3 == 1 ? 1 : (data.data[i].badgeId % 3 == 2 ? 2 : 3);
+            console.log(check);
+            data.data[i].url = url;
+            data.data[i].check = check;
+            data.data[i].tier = tier;
+            badgeList.value.push(data.data[i]);
+            console.log(data.data[i]);
+          }
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    },
+    (error) => {
+      console.log(error);
+    }
+  );
+});
 </script>
 
-<style></style>
+<style>
+.flex-container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between; /* 각 이미지를 컨테이너의 양 끝에 정렬 */
+}
+
+.flex-item {
+  flex-basis: calc(
+    25% - 10px
+  ); /* 한 줄에 4개씩 배치하므로 각 항목의 너비는 25%로 설정 */
+  margin-bottom: 20px; /* 이미지 아래에 여백 추가 */
+}
+
+.flex-item img {
+  width: 75%; /* 이미지를 꽉 채웁니다. */
+  height: auto; /* 가로 비율을 유지하면서 세로 비율을 자동으로 조정합니다. */
+}
+</style>
