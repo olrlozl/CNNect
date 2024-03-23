b22c521d7c6b63e2b67f018e8af5c1da350eac97
olrlozl <thakd221@naver.com>
2024-03-23 16:07:25 +0900

    Feat: 영어사전 스크래핑 실패 시 3번까지 재시도

--- frontend/src/api/scraping.js
+++ frontend/src/api/scraping.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 import cheerio from "cheerio";
 
-const getDict = async (searchWord) => {
+const getDict = async (searchWord, retryCount = 0) => {
     try {
         const html = await axios.get(`http://cors-anywhere.herokuapp.com/https://dic.daum.net/search.do?q=${searchWord}`);
         let list = [];
         const $ = cheerio.load(html.data);
         const elements = $("ul.list_search").first().find("span.txt_search");
-        elements.map((i, element) => {
+        elements.each((i, element) => {
             list[i] = {
                 num: i + 1,
                 mean: $(element).text(),
@@ -15,7 +15,11 @@ const getDict = async (searchWord) => {
         });
         return list;
     } catch (error) {
-        console.error(error);
+        if (retryCount < 3) {
+            return await getDict(searchWord, retryCount + 1);
+        } else {
+            return null;
+        }
     }
 };
 
