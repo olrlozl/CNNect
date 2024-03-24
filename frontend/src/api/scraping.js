07818b2ede475111e6662784504f45f5752380c5
olrlozl <thakd221@naver.com>
2024-03-24 16:01:52 +0900

    Feat: 로컬 캐시를 이용해 동일 영단어 재검색 시 조회 속도 향상

--- frontend/src/api/scraping.js
+++ frontend/src/api/scraping.js
@@ -1,8 +1,28 @@
 import axios from "axios";
 import cheerio from "cheerio";
 
+const cache = {};
+
+const getFromCache = (key) => {
+    const item = cache[key];
+    if (item && (item.expire > Date.now())) {
+        return item.data;
+    }
+    return null;
+};
+
+const saveToCache = (key, data, ttl) => {
+    const expire = Date.now() + ttl * 1000;
+    cache[key] = { data, expire };
+};
+
 const getDict = async (searchWord, retryCount = 0) => {
     try {
+        const cachedData = getFromCache(searchWord);
+        if (cachedData) {
+            return cachedData;
+        }
+
         const html = await axios.get(`/daum/search.do?q=${searchWord}`);
         let list = [];
         const $ = cheerio.load(html.data);
@@ -13,7 +33,11 @@ const getDict = async (searchWord, retryCount = 0) => {
                 mean: $(element).text(),
             };
         });
+
+        saveToCache(searchWord, list, 86400); // 캐시 만료 시간 1일
+
         return list;
+
     } catch (error) {
         if (retryCount < 3) {
             return await getDict(searchWord, retryCount + 1);
