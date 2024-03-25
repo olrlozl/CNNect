7e502589ee32f92d24bf315108b1d31ff8a7b67b
최ë동헌 <ehdgjs456@naver.com>
2024-03-25 17:51:22 +0900

    feat: 단어장, 학습 동영ì상 화면구성

--- frontend/src/components/history/Voca.vue
+++ frontend/src/components/history/Voca.vue
@@ -1,38 +1,76 @@
 <template>
-    <table>
-        <tr v-for="wordData in currentPageWordList " :key="wordData.word">
-            <th class="word"> {{ wordData.word }} </th>
-            <th class="mean"> {{ wordData.mean }} </th>
-        </tr>
-    </table>
-    <div>
-      <button @click="previousPage" :disabled="currentPage === 0">이전</button>
-      <span>Page {{ currentPage + 1 }}</span>
-      <button @click="nextPage" :disabled="currentPage === totalPages-1">다음</button>
+    <div class  = "word-list">
+        <ul>
+            <li v-if="currentPageWordList.length !== 0 " v-for="wordData in currentPageWordList " :key="wordData.word" class = "item">
+                <div class="word-item">
+                    <div class="word"> {{ wordData.word }} </div>
+                    <div class="mean"> {{ wordData.mean }} </div>
+                </div>
+                <div class="del">
+                    <span class="material-symbols-outlined" @click="delWord(wordData.wordListId)">
+                        cancel
+                    </span>
+                </div>
+            </li>
+            <li v-else class="blank ">저장된 단어가 존재하지 않습니다.</li>
+        </ul>       
+    </div>
+    <div class="page">
+        <div>
+            <span class="material-symbols-outlined" @click="previousPage" :disabled="currentPage === 0">
+                arrow_back_ios
+            </span>
+        </div>
+        <div class="page-container">
+            <span v-for="page in visiblePages" :key="page" @click="goToPage(page)" :class="{ active: currentPage === page - 1 }" class="page-item">{{ page }}</span>
+        </div>
+
+        <div>
+            <span class="material-symbols-outlined" @click="nextPage" :disabled="currentPage === totalPages-1">
+                arrow_forward_ios
+            </span>
+        </div>  
     </div>
 </template>
 
 <script setup>
-    import {defineProps, ref ,computed } from 'vue';
+    import { ref , computed , defineProps } from 'vue';
 
-    defineProps({
-        history: Object
+    const props = defineProps({
+        wordHistory: Object
     })
     
-    const sortedHistory = ref([]);
+    // const sortedHistory = ref([]);
 
-    // 이부분 이렇게가 맞나???
-    sortedHistory.value = history.wordList.sort((a, b) => a.word.localeCompare(b.word));
+    // sortedHistory.value = props.wordHistory.wordList;
     
     const currentPage = ref(0);
     const wordPerPage = 5;
 
-    const totalPages = computed(() => Math.ceil(sortedHistory.length / wordPerPage));
+    const totalPages = computed(() => Math.ceil(props.wordHistory.wordList.length / wordPerPage));
 
     const currentPageWordList = computed(() => {
         const startIndex = currentPage.value * wordPerPage;
-        return sortedHistory.slice(startIndex, startIndex + wordPerPage);
+        return props.wordHistory.wordList.slice(startIndex, startIndex + wordPerPage);
     });
+
+
+    const visiblePages = computed(() => {
+        const pages = [];
+        const currentPageValue = currentPage.value + 1;
+
+        const startPage = Math.max(1, currentPageValue - 2); // 현재 페이지를 중심으로 앞쪽에 최대 2개의 페이지를 보여줍니다.
+        const endPage = Math.min(totalPages.value, currentPageValue + 2); // 현재 페이지를 중심으로 뒷쪽에 최대 2개의 페이지를 보여줍니다.
+       
+
+
+        for (let i = startPage; i <= endPage; i++) {
+            pages.push(i);
+        }
+
+        return pages;
+    });
+
 
     function nextPage() {
         if (currentPage.value < totalPages.value - 1) {
@@ -46,9 +84,91 @@
         }
     }
 
+    function goToPage(page) {
+        currentPage.value = page - 1;
+    }
+
+function delWord(wordListId) {
+    const index = props.wordHistory.wordList.findIndex(item => item.wordListId === wordListId);
+    if (index !== -1) {
+        props.wordHistory.wordList.splice(index, 1); // 배열에서 삭제
+        if (props.wordHistory.wordList.slice(currentPage.value * wordPerPage, currentPage.value * wordPerPage + wordPerPage).length === 0 && currentPage.value > 0) {
+            currentPage.value--;
+        }
+    }
+}
+
 
 </script>
 
 <style scoped>
+.item{
+    display: flex;
+    flex-direction: row;
+}
+.word-item {
+    border: 1px solid #e2e2e2;
+    box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.1);
+    border-radius: 5px;
+    margin: 15px;
+    padding: 0 10px;
+    width: 90%;
+    display: flex;
+    flex-direction: row;
+}
+.del{
+    color: #CC0000;
+    display: flex;
+    justify-content: center; /* 수평 가운데 정렬 */
+    align-items: center; /* 수직 가운데 정렬 */
+    margin : 10px;
+    cursor: pointer;
+}
 
+.word {
+    margin : 10px;
+    color: #CC0000;
+    font-weight: 600;
+    width: 20%;
+    border-right: #E3E3E3 1px solid;
+}
+.mean {
+    margin : 10px;
+    width: 80%;
+}
+.word-list{
+    height: 80%;
+    overflow-y: scroll;
+}
+.active{
+    color: #CC0000;
+}
+.page-container{
+    justify-content: center; /* 수평 가운데 정렬 */
+    align-items: center; /* 수직 가운데 정렬 */
+    text-align: center;
+    width: 15%;
+}
+.page{
+    display: flex;
+    justify-content: center; /* 수평 가운데 정렬 */
+    align-items: center; /* 수직 가운데 정렬 */
+    bottom: 8%;
+    left: 50%;
+    width: 100%; /* 페이지 네비게이션의 너비를 조정할 수 있습니다. */
+    background-color: #fff; /* 필요에 따라 배경색을 지정하세요 */
+    padding-top: 1%;
+
+}
+.page-item {
+  margin: 0 10px; 
+  cursor: pointer;
+}
+.blank{
+    margin-top: 20%;
+    color: #CC0000;
+    text-align: center;
+    font-size: 200%;
+    font-style: bold;
+}
 </style>
--- frontend/src/views/history/HistoryView.vue
+++ frontend/src/views/history/HistoryView.vue
@@ -1,18 +1,224 @@
 <template>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/reset-css@5.0.1/reset.css" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+
     <body>
-        <div>
-            
-        </div>
-        <div>
-
-        </div>
-
+        <div class="section-box">
+            <div class="section2">
+                <div class="tabmenu out-tabmenu">
+                    <ul>
+                        <li id="tab1" class="btnCon">
+                            <input type="radio" checked name="tabmenu" id="tabmenu1">
+                            <label for="tabmenu1">동영상</label>
+                            <div class="tabCon">
+                                <h1 class="text-2xl font-bold p-3">학습 진행중인 뉴스</h1>
+                                <div class="learning-video">
+                                    <div class="Carousel-btn">
+                                        <span class="material-symbols-outlined" @click="changeVideoOrder('backward')">
+                                            arrow_back_ios
+                                        </span>
+                                    </div>   
+                                    <div >
+                                        <learningVideo :curVideo="curVideo" />
+                                    </div>
+                                    <div class="Carousel-btn">
+                                        <span class="material-symbols-outlined" @click="changeVideoOrder('foreward')">
+                                            arrow_forward_ios
+                                        </span>
+                                    </div>
+                                </div>
+                                <div >
+                                   <completionVideo :completionVideoList = "videoHistory.completionVideoList" />
+                                </div>
+                            </div>
+                        </li>
+                        <li id="tab2" class="btnCon">
+                            <input type="radio" name="tabmenu" id="tabmenu2">
+                            <label for="tabmenu2">단어장</label>
+                            <div class="tabCon" style="overflow-y: hidden;">
+                                <Voca :wordHistory="wordHistory" />
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>    
     </body>
 </template>
 
 <script setup>
+import Voca from '@/components/history/Voca.vue';
+import completionVideo from '@/components/history/completionVideo.vue';
+import learningVideo from '@/components/history/learningVideo.vue';
+
+import { ref } from 'vue';
+import { onMounted } from 'vue';
+import { insertVideoHistory, insertWordHistory } from '@/api/history';
+
+const videoHistory = ref({
+    learningViedeoList: [],
+    completionVideoList: []
+})
+
+const wordHistory = ref({
+    wordList: []
+})
+
+let totalVideos = 0;
+
+let currentOrder = 0;
+
+const curVideo = ref({
+    order: "",
+    videoTitle: "",
+    videoUrl: "",
+    lastSentence: "",
+    videoLevel: "",
+    completedSentenceNum: "",
+    totalSentenceNum: ""
+})
+
+function setCurVideo (idx) {
+    curVideo.value.order = videoHistory.value.learningViedeoList[idx].order;
+    curVideo.value.videoTitle = videoHistory.value.learningViedeoList[idx].videoTitle;
+    curVideo.value.videoUrl = videoHistory.value.learningViedeoList[idx].videoUrl;
+    curVideo.value.lastSentence = videoHistory.value.learningViedeoList[idx].lastSentence;
+    curVideo.value.videoLevel = videoHistory.value.learningViedeoList[idx].videoLevel;
+    curVideo.value.completedSentenceNum = videoHistory.value.learningViedeoList[idx].completedSentenceNum;
+    curVideo.value.totalSentenceNum = videoHistory.value.learningViedeoList[idx].totalSentenceNum;
+}
+
+function changeVideoOrder(direction) {
+    if (direction === 'backward') {
+        if (currentOrder > 0) {
+            currentOrder -= 1;
+            setCurVideo(currentOrder);
+        } else {
+            currentOrder = totalVideos-1;
+            setCurVideo(totalVideos-1);
+        }
+    } else if (direction === 'foreward') {
+        if (currentOrder < totalVideos-1) {
+            currentOrder += 1;
+            setCurVideo(currentOrder);
+        } else {
+            currentOrder = 0;
+            setCurVideo(0);
+        }
+    }
+}
+
+
+onMounted(async () => {
+    videoHistory.value = await insertVideoHistory();
+    wordHistory.value = insertWordHistory();
+    totalVideos = videoHistory.value.learningViedeoList.length;
+    setCurVideo(0);
+})
+
 </script>
 
 <style>
+/* section-box */
+.section-box {
+    display: flex;
+    flex-flow: wrap;
+}
 
+/* section-box / section1 */
+.section1 {
+    padding: 20px;
+    flex-grow: 1;
+    flex-basis: 480px;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+}
+
+/* section-box / section2 */
+.section2 {
+    flex-grow: 1;
+    flex-basis: 300px;
+    height: 600px;
+    padding: 20px;
+}
+.tabmenu{ 
+    margin: 0 auto;
+}
+.tabmenu ul{
+    position: relative;
+}
+.tabmenu > ul > li{
+    display: inline-block;
+    width:33.33%;
+    max-width: 130px;
+    text-align:center;
+    background :#f9f9f9;
+    line-height:40px;
+    border-radius: 10px 10px 0 0;
+    box-shadow: 3px 0 10px rgba(0, 0, 0, 0.25);
+    margin-right: 10px;
+    
+}
+.tabmenu label{
+    display:block;
+    width:100%; 
+    height: 40px;
+    line-height:40px;
+    cursor: pointer;
+}
+.tabmenu input{
+    display:none;
+}
+
+.tabCon{
+    background-color: #ffffff;
+    display: none; 
+    width: 100%;
+    text-align: left; 
+    position: absolute; 
+    left: 0;
+    top: 40px; 
+    box-sizing: border-box; 
+    border : 1px solid #e6e6e6;
+    filter: drop-shadow(2px 2px 3px rgba(0, 0, 0, 0.25));
+    border-radius: 0 10px 10px 10px;
+    height: 500px;
+    overflow-y: scroll;
+}
+.learning-video{
+    display: flex;
+    justify-content: center; /* 수평 가운데 정렬 */
+    align-items: center; /* 수직 가운데 정렬 */
+    flex-direction: row;
+}
+.tabmenu input:checked ~ label{
+    font-weight: 800;
+    padding-top: 5px;
+    font-size: 17px;
+    background:#f9f9f9;
+    border-radius: 10px 10px 0 0;
+    box-shadow: 3px 0px 10px rgba(0, 0, 0, 0.25);
+    transform: translateY(-5px);
+    height: 50px;
+}
+.tabmenu input:checked ~ label:hover{
+    background-color: #f0f0f0;
+}
+.btnCon:hover{
+    background-color: #f0f0f0;
+}
+.tabmenu input:checked ~ .tabCon{
+    display:block;
+}
+.material-symbols-outlined{
+    color: #CC0000;
+    cursor: pointer;
+    padding: 10px;
+}
+.Carousel-btn{
+    display: flex;
+    justify-content: center; /* 수평 가운데 정렬 */
+    align-items: center; /* 수직 가운데 정렬 */
+}
 </style>
