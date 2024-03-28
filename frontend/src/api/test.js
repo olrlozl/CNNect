e48f130732d767e879663c096d4b7c1a05fec5a5
Haeun Kim <gkdms1120s@gmail.com>
2024-03-28 14:45:48 +0900

    Feat : 슬래시 추가

--- frontend/src/api/test.js
+++ frontend/src/api/test.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { ref } from "vue";
 
-const REST_DATA_API = `https:/j10a507.p.ssafy.io/data/level`;
+const REST_DATA_API = `https://j10a507.p.ssafy.io/data/level`;
 // const REST_DATA_API = `http://localhost:5000/data/level`;
 
 async function getUserLevelTestList(level, success, fail) {
