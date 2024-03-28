d51cd658a6698e39a9fcda65c8bdc9dff66708a0
Haeun Kim <gkdms1120s@gmail.com>
2024-03-28 17:09:33 +0900

    Feat: 크롤링 스케쥴러 즉시로딩

--- data/app.py
+++ data/app.py
@@ -15,7 +15,7 @@ def job_function():
 scheduler = BackgroundScheduler(daemon=True)
 
 # 1일마다 실행
-scheduler.add_job(job_function, 'interval', days=1)
+scheduler.add_job(job_function, 'interval', days=1, next_run_time=datetime.now())
 scheduler.start()
 
 app = Flask(__name__)
