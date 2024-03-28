bef7b2ee6e45c74aa4d590813746485f62955a28
Haeun Kim <gkdms1120s@gmail.com>
2024-03-28 16:20:24 +0900

    Feat: level 저장 로직 임시저장

--- frontend/src/api/test.js
+++ frontend/src/api/test.js
@@ -1,11 +1,31 @@
 import axios from "axios";
-import { ref } from "vue";
+import { localAxios } from "./http";
+
+const local = localAxios();
+const config = {
+  headers: {
+    Authorization: "Bearer " + localStorage.getItem("accessToken"),
+  },
+};
+
+const config2 = {
+  headers: {
+    Authorization: "Bearer ",
+  },
+};
 
 const REST_DATA_API = `https://j10a507.p.ssafy.io/data/level`;
-// const REST_DATA_API = `http://localhost:5000/data/level`;
 
 async function getUserLevelTestList(level, success, fail) {
   axios.get(`${REST_DATA_API}/user/${level}`).then(success).catch(fail);
 }
 
-export { getUserLevelTestList };
+function setUserLevelToken(param, success, fail) {
+  local.patch(`user/update/level`, param, config).then(success).catch(fail);
+}
+
+function setUserLevelNotToken(param, success, fail) {
+  local.patch(`user/update/level`, param, config2).then(success).catch(fail);
+}
+
+export { getUserLevelTestList, setUserLevelToken, setUserLevelNotToken };
