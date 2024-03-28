1df53045fbab3eb5fae01ed03c3c75b4cc28b9c4
Haeun Kim <gkdms1120s@gmail.com>
2024-03-28 13:10:19 +0900

    Feat : userTest url 서버 url로 변경

--- frontend/src/api/test.js
+++ frontend/src/api/test.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 import { ref } from "vue";
 
-// const REST_DATA_API = `https:/j10a507.p.ssafy.io/data/level`;
-const REST_DATA_API = `http://localhost:5000/data/level`;
+const REST_DATA_API = `https:/j10a507.p.ssafy.io/data/level`;
+// const REST_DATA_API = `http://localhost:5000/data/level`;
 
 async function getUserLevelTestList(level, success, fail) {
   axios.get(`${REST_DATA_API}/user/${level}`).then(success).catch(fail);
