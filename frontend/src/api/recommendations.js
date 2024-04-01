6afacb742847740854070ec3e498240770cebfff
noowadc <cha12608@naver.com>
2024-04-01 13:22:27 +0900

    Style : 메세지 확인

--- frontend/src/api/recommendations.js
+++ frontend/src/api/recommendations.js
@@ -16,7 +16,7 @@ async function fetchRecommendations() {
       const response = await local.get(`${url}/all`, config);
 
       return response.data.data;
-      
+      console.log(response.data.data);
     } catch (error) {
       console.error(error);
     }
